02ac991d5c8cbc07c1e1eefc2fad9679f8cec2d0
tuftscs <tuftscs@tuftscsvm.tuftscsvm>
2017-06-13 16:26:19 -0400

    json data loads correctly

--- messages/lab.js
+++ messages/lab.js
@@ -1,13 +1,13 @@
 // Your JavaScript goes here...
 function loadJson() {
 	request = new XMLHttpRequest();
-	request.open("GET", data.json, true);
-	request.onreadstatefunction = function(){
+	request.open("GET", "data.json", true);
+	request.onreadystatechange = function(){
 		if(request.readyState == 4 && request.status == 200){
-			data = JSON.parse(request.responseText);
-			message = "";
-			for(i = 0; i < data.length; i++)
-				message += "<p>" + data[i].content + " " + parsed[i].username + "</p>";
+			elements = JSON.parse(request.responseText);
+			message = document.getElementById("messages");
+			for(i = 0; i < elements.length; i++)
+      			message.innerHTML += "<p>" + elements[i].content + " " + elements[i].username + "</p>";
 		}
 	}
 	request.send();
